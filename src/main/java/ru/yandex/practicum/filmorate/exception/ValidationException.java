1954907df767e8b52d10f8c83c52060cf52a46c7
Dima <dimarastvorcev6123@gmail.com>
2025-11-22 19:15:26 +0300

    refactor

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends RuntimeException {
     private String message;
+
     public ValidationException(String message) {
         this.message = message;
     }
